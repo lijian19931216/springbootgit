b0b49152008e2fefdf95f1922486a227836393b3
上海陆鹰-李剑 <928229131@qq.com>
2018-07-28 12:41:28 +0800

    eclipse 第三次提交
    (cherry picked from commit 67dc3ae378fa5f42f0296e8e833ba78d90f2bd04)
    
    # Conflicts:
    #	src/main/java/com/luying/springbootgit/Test.java

--- src/main/java/com/luying/springbootgit/Test.java
+++ src/main/java/com/luying/springbootgit/Test.java
@@ -6,13 +6,13 @@ package com.luying.springbootgit;
  * @create: 2018-07-28
  **/
 public class Test {
-	//eclipse 第一次提交
-	//eclipse 第二次提交
-	//eclipse 第三次提交
-	//eclipse 第四次提交
-	//idea 第一次提交
-	//idea 第二次提交
-	//idea 第三次提交
-	//idea 第四次提交
+	/*
+	eclipse 第一次提交
+	eclipse 第二次提交
+	eclipse 第三次提交
+	idea 第一次提交
+	idea 第二次提交
+	idea 第三次提交
+	*/
 
 }
