259d62164b6e01cf272c48aacc6b3e5b20bc6c5e
上海陆鹰-李剑 <928229131@qq.com>
2018-07-28 11:50:47 +0800

    idea 第1次提交

--- src/main/java/com/luying/springbootgit/Test.java
+++ src/main/java/com/luying/springbootgit/Test.java
@@ -6,14 +6,5 @@ package com.luying.springbootgit;
  * @create: 2018-07-28
  **/
 public class Test {
-	//eclipse 第一次提交
-	//eclipse 第二次提交
-    //eclipse 第三次提交
-    //eclipse 第四次提交
-	//eclipse 第五次提交
-    //idea 第一次提交
-    //idea 第二次提交
-    //idea 第三次提交
-    //idea 第四次提交
-    //idea 第五次提交
+	//idea 第一次提交
 }
