2b36a8564ff55146241ef3d090078da422a365c5
lijian <928229131@qq.com>
2019-05-31 01:09:38 +0800

    32

--- src/main/java/com/luying/springboot/controller/TestController.java
+++ src/main/java/com/luying/springboot/controller/TestController.java
@@ -5,5 +5,14 @@ package com.luying.springboot.controller;
  * @author: lijian
  * @create: 2019-02-02
  **/
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @RequestMapping("/hello")
+    public String hh(){
+        return "hello lijian";
+    }
 }
